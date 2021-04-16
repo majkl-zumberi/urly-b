5b2a19359223e5e636ddaad5f6f1c330842590fc
majkl zumberi <majklzumberi00@gmail.com>
2021-04-16 10:22:17 +0200

    fix(module): :wrench: update app module | mongoose  & shortUrl module initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ShortUrlModule } from './short-url/short-url.module';
 @Module({
-  imports: [],
+  imports: [
+    ShortUrlModule,
+    MongooseModule.forRoot('mongodb://localhost/urly')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
