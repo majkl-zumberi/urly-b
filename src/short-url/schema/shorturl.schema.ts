fa68db151f858f34ad1e469a1ecd30157977d019
majkl zumberi <majklzumberi00@gmail.com>
2021-04-17 12:04:08 +0200

    fix(schema): :label: update schema (add device ref)

--- src/short-url/schema/shorturl.schema.ts
+++ src/short-url/schema/shorturl.schema.ts
@@ -1,7 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import * as shortid from 'shortid';
-
+import * as mongoose from 'mongoose';
+import { Device } from '../../device/schema/device.schema';
 export type ShortUrlDocument = ShortUrl & Document;
 
 @Schema()
@@ -14,5 +15,12 @@ export class ShortUrl {
 
     @Prop({default:0})
     clicks:number;
+
+    @Prop({
+        type: [{ type: [mongoose.Schema.Types.ObjectId], ref: 'Device' }],
+        required: false,
+      })
+    devices: [Device];
 }
 export const ShortUrlSchema = SchemaFactory.createForClass(ShortUrl);
+ShortUrlSchema.set('toJSON', { virtuals: true });
